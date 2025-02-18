07bd9982cd10bfe18b626e2b8abbb61e8daf6d29
jcbowyer <jcbowyer@hotmail.com>
2025-02-18 17:10:11 -0600

    feat: Update Kyuubi and Spark configurations, improve Docker setup, and remove unused docker-compose file

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -6,3 +6,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
