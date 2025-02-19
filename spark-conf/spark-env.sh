1feedf1f0809ebb077b3217a423b331ec3b9562b
jcbowyer <jcbowyer@hotmail.com>
2025-02-18 21:42:55 -0600

    feat: Add Kyuubi service to Docker Compose and update configurations for improved integration

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -17,3 +17,7 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
