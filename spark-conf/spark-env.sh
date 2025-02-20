156263d7e9d19862d104961cf019af376943685d
jcbowyer <jcbowyer@hotmail.com>
2025-02-20 13:16:11 -0600

    fix: Update workspace folder in devcontainer and clean up duplicate LD_PRELOAD entries in spark-env.sh; enhance test notebook output

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -61,3 +61,7 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
