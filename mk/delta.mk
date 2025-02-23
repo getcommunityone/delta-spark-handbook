4a4dbc5ae3d93f1c64fc3137d8cd9899cda5c735
jcbowyer <jcbowyer@hotmail.com>
2025-02-22 20:38:09 -0600

    feat: Update dependencies for Delta Spark and PySpark; add Delta Sharing configuration and enhance Dockerfile for Kyuubi setup

--- mk/delta.mk
+++ mk/delta.mk
@@ -18,12 +18,13 @@ delta-download-jars: create-dirs
 	@mkdir -p delta-jars
 	@sudo chown -R $(CURRENT_USER):$(CURRENT_USER) delta-jars/
 	@sudo chmod -R 755 delta-jars/
-	@rm -f delta-jars/delta-spark_2.12-3.2.1.jar
-	@rm -f delta-jars/delta-storage-3.2.1.jar
+	@rm -f delta-jars/delta-spark_2.12-3.3.0.jar
+	@rm -f delta-jars/delta-storage-3.3.0.jar
 	@rm -f delta-jars/hadoop-aws-$(HADOOP_AWS_VERSION).jar
 	@rm -f delta-jars/aws-java-sdk-bundle-$(AWS_SDK_VERSION).jar
-	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-spark_2.12/3.2.1/delta-spark_2.12-3.2.1.jar
-	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-storage/3.2.1/delta-storage-3.2.1.jar
+	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-spark_2.12/3.3.0/delta-spark_2.12-3.3.0.jar
+	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-storage/3.3.0/delta-storage-3.3.0.jar
 	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/$(HADOOP_AWS_VERSION)/hadoop-aws-$(HADOOP_AWS_VERSION).jar
 	wget -P delta-jars https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/$(AWS_SDK_VERSION)/aws-java-sdk-bundle-$(AWS_SDK_VERSION).jar
+	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hive/hive-jdbc/3.1.3/hive-jdbc-3.1.3.jar
 	@echo "JARs downloaded successfully"
