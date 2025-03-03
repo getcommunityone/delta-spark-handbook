c455c7d9699f37309e99bcb2658a4e224a30ee92
jcbowyer <jcbowyer@hotmail.com>
2025-03-03 11:08:03 -0600

    feat: Update homepage styles and images, add new SVG icons

--- mk-docs/src/components/HomepageFeatures/index.js
+++ mk-docs/src/components/HomepageFeatures/index.js
@@ -1,49 +1,73 @@
+import React from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import styles from './styles.module.css'; // Make sure this path is correct
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Ingestion',
+    Svg: require('@site/static/img/LetterI.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Data Load',
+    Svg: require('@site/static/img/LetterD.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Enrichment',
+    Svg: require('@site/static/img/LetterE.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+
+      </>
+    ),
+  },
+  {
+    title: 'Analysis',
+    Svg: require('@site/static/img/LetterA.svg').default,
+    description: (
+      <>
+
+      </>
+    ),
+  },
+  {
+    title: 'Story Telling Visualization',
+    Svg: require('@site/static/img/LetterS.svg').default,
+    description: (
+      <>
+
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function FeatureCard({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+    <div className={clsx('col col--2')}>
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          {Svg && (
+            <div className={styles.cardImage}>
+              <Svg className={styles.featureSvg} role="img" />
+            </div>
+          )}
+          <div className={styles.cardTitle}>
+            <Heading as="h3">{title}</Heading>
+          </div>
+        </div>
+        <div className={styles.cardBody}>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
@@ -55,7 +79,7 @@ export default function HomepageFeatures() {
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <FeatureCard key={idx} {...props} />
           ))}
         </div>
       </div>
