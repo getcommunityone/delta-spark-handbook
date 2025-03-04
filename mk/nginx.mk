6f9e1ea156efdd799cf5bf08b27ada1a04537020
jcbowyer <jcbowyer@hotmail.com>
2025-03-04 12:40:49 -0600

    feat: Enhance Nginx setup for Docusaurus and add MinIO configuration in Docker Compose

--- mk/nginx.mk
+++ mk/nginx.mk
@@ -1,4 +1,4 @@
-# Makefile for setting up a web server with Nginx to serve a Docusaurus site
+# Makefile for setting up a web server with Nginx to serve a Docusaurus site and MinIO object storage
 
 # Variables
 SERVER_IP = 136.239.110.18
@@ -8,10 +8,9 @@ DOCUSAURUS_DIR = mk-docs
 NGINX_SITES_AVAILABLE = /etc/nginx/sites-available
 NGINX_SITES_ENABLED = /etc/nginx/sites-enabled
 BUILD_DIR = $(DOCUSAURUS_DIR)/build
-NGINX_CONF = $(NGINX_SITES_AVAILABLE)/default
+NGINX_CONF = $(NGINX_SITES_AVAILABLE)/$(DOMAIN)
+MINIO_DOMAIN = minio.communityone.com
 
-# Default rule to build and deploy everything
-all: nginx-install nginx-build nginx-deploy nginx-restart
 
 # Install Nginx
 nginx-install:
@@ -21,9 +20,38 @@ nginx-install:
 nginx-build:
 	cd $(DOCUSAURUS_DIR) && npm install && npm run build
 
+
 # Deploy the Docusaurus build to the serving directory
 nginx-deploy:
-	sudo rsync -av --delete $(BUILD_DIR)/ /var/www/html/
+	sudo rsync -av --delete $(BUILD_DIR)/ /var/www/$(DOMAIN)/
+
+# Configure Nginx to serve the Docusaurus site and proxy to MinIO
+# Configure Nginx to serve the Docusaurus site and proxy to MinIO
+nginx-config:
+    # Back up the existing Nginx configuration if it exists
+    if [ -f $(NGINX_CONF) ]; then \
+        sudo cp $(NGINX_CONF) $(NGINX_CONF).bak; \
+    fi
+    sudo bash -c 'echo "server { \
+        listen 80; \
+        listen [::]:80; \
+        server_name $(DOMAIN) www.$(DOMAIN); \
+        location / { \
+            root /var/www/$(DOMAIN); \
+            index index.html index.htm; \
+            try_files \$$uri \$$uri/ =404; \
+        } \
+        location /minio/ { \
+            proxy_set_header Host \$$host; \
+            proxy_pass http://localhost:9001; \
+        } \
+        location /minio-storage/ { \
+            proxy_set_header Host \$$host; \
+            proxy_pass http://localhost:9000; \
+        } \
+    }" > $(NGINX_CONF)'
+    sudo ln -sf $(NGINX_CONF) $(NGINX_SITES_ENABLED)/$(DOMAIN)
+
 
 # Restart Nginx to apply configuration changes
 nginx-restart:
@@ -35,12 +63,8 @@ nginx-clean:
 
 # Obtain SSL certificate using Certbot
 nginx-certbot-setup:
-	sudo certbot --nginx -d $(DOMAIN) -d www.$(DOMAIN) --non-interactive --agree-tos -m $(ADMIN_EMAIL)
+	sudo certbot --nginx -d $(DOMAIN) -d www.$(DOMAIN) -d $(MINIO_DOMAIN) --non-interactive --agree-tos -m $(ADMIN_EMAIL) --redirect
 
 # Set up automatic certificate renewal
 nginx-certbot-renew:
 	echo "0 0,12 * * * root certbot renew --quiet" | sudo tee -a /etc/crontab > /dev/null
-
-
-
-
