4775f5184578853909ee4246d5ad4a65774f9392
jcbowyer <jcbowyer@hotmail.com>
2025-03-05 12:07:38 -0600

    feat: Update .gitignore to exclude spark-env.sh and modify spark-env.sh for LD_PRELOAD settings

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -131,3 +131,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
