a22d173b4ab357fe8381fd56c0793879f798e6f4
jcbowyer <jcbowyer@hotmail.com>
2025-03-06 18:52:59 +0000

    chore: update .gitignore to include spark-env.sh and *.pid; bump Python version in example notebook

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -172,3 +172,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
