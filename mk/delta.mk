81280408662fd81506d4295ca824f7ef5387040d
jcbowyer <jcbowyer@hotmail.com>
2025-03-05 21:27:48 -0600

    feat: Revise documentation and enhance styling; update JAR download process

--- mk/delta.mk
+++ mk/delta.mk
@@ -15,7 +15,7 @@ delta-create-dirs:
 	@sudo chmod -R 755 delta-jars/
 
 delta-download-jars: delta-create-dirs
-	@echo "Downloading Delta Lake and AWS JARs..."
+	@echo "Downloading Delta Lake, AWS, and Stax2 JARs..."
 	@mkdir -p delta-jars
 	@sudo chown -R $(CURRENT_USER):$(CURRENT_USER) delta-jars/
 	@sudo chmod -R 755 delta-jars/
@@ -23,12 +23,38 @@ delta-download-jars: delta-create-dirs
 	@rm -f delta-jars/delta-storage-3.3.0.jar
 	@rm -f delta-jars/hadoop-aws-$(HADOOP_AWS_VERSION).jar
 	@rm -f delta-jars/aws-java-sdk-bundle-1.12.782.jar
+	@rm -f delta-jars/hive-jdbc-3.1.3.jar
+	@rm -f delta-jars/hadoop-common-3.4.1.jar
+	@rm -f delta-jars/hadoop-client-3.4.1.jar
+	@rm -f delta-jars/hadoop-auth-3.4.1.jar
+	@rm -f delta-jars/woodstox-core-6.2.6.jar
+	@rm -f delta-jars/stax2-api-4.2.1.jar  # Remove old stax2-api if it exists
+	@rm -f delta-jars/commons-configuration2-2.7.jar
+
+	# Download Delta Lake JARs
 	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-spark_2.12/3.3.0/delta-spark_2.12-3.3.0.jar
 	wget -P delta-jars https://repo1.maven.org/maven2/io/delta/delta-storage/3.3.0/delta-storage-3.3.0.jar
+
+	# Download Hadoop AWS and AWS SDK JARs
 	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/$(HADOOP_AWS_VERSION)/hadoop-aws-$(HADOOP_AWS_VERSION).jar
 	wget -P delta-jars https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.782/aws-java-sdk-bundle-1.12.782.jar
+
+	# Download Hive JDBC JAR
 	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hive/hive-jdbc/3.1.3/hive-jdbc-3.1.3.jar
+
+	# Download Hadoop Common, Client, Auth JARs
 	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-common/3.4.1/hadoop-common-3.4.1.jar
-	wget -P delta-jars https://repo1.maven.org/maven2/com/fasterxml/woodstox/woodstox-core/6.2.6/woodstox-core-6.2.6.jar  
+	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-client/3.4.1/hadoop-client-3.4.1.jar
+	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-auth/3.4.1/hadoop-auth-3.4.1.jar
+
+	# Download Woodstox core JAR
+	wget -P delta-jars https://repo1.maven.org/maven2/com/fasterxml/woodstox/woodstox-core/6.2.6/woodstox-core-6.2.6.jar
+
+	# Download Stax2 API JAR (fix for XMLInputFactory2)
+	wget -P delta-jars https://repo1.maven.org/maven2/org/codehaus/woodstox/stax2-api/4.2.1/stax2-api-4.2.1.jar
+
+	@echo "Downloading commons-configuration2 JAR..."
+	wget -P delta-jars https://repo1.maven.org/maven2/org/apache/commons/commons-configuration2/2.7/commons-configuration2-2.7.jar
+
 
 	@echo "JARs downloaded successfully"
