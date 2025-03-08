a338314d41701ba7cd9fce1ee51ba68dfd8dae2d
jcbowyer <jcbowyer@hotmail.com>
2025-03-08 20:13:53 +0000

    fix: correct hiveserver2.pid value and remove duplicate LD_PRELOAD entries in spark-env.sh

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -202,3 +202,6 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
