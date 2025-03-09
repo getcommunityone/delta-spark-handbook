fba5e9b696b6afce436675e6daa846e264e37f2f
jcbowyer <jcbowyer@hotmail.com>
2025-03-09 19:25:52 +0000

    feat: enhance EHR data ingestion with MinIO support, update S3 endpoint, and add MinIO configuration Makefile

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -177,3 +177,7 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
