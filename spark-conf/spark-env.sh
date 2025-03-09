dca997d8bbec5261a97dca1d59645eb7f487b00f
jcbowyer <jcbowyer@hotmail.com>
2025-03-08 20:23:44 -0600

    fix: remove duplicate LD_PRELOAD entries in spark-env.sh

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -171,3 +171,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
