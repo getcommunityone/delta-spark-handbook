96e8acc7df2da2a0f85ff42ea17e3b2d6bc3418d
jcbowyer <jcbowyer@hotmail.com>
2025-03-09 17:40:45 -0500

    feat: update Spark configuration for EHR data ingestion, adjust Python version in notebook, and clean up devcontainer ports

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -181,3 +181,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
